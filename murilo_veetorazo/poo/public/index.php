17d13d7b46fcf2239d878b6c10dc2043665fba6b
MuriloVetrin <murilovtrindade@gmail.com>
2022-11-01 22:20:08 -0300

    novo

--- murilo_veetorazo/poo/public/index.php
+++ murilo_veetorazo/poo/public/index.php
@@ -5,8 +5,12 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Unialfa\Poo\FormaBolo;
 
 //var_dump($_REQUEST["tempoDeForno"]);exit;
-$tempoDeForno = $_REQUEST["tempoDeForno"];
+$tempoDeForno = $_REQUEST["tempoDeForno"] ?? null;
 
 $turmaBolo = new FormaBolo();
 $turmaBolo->fazerBolo();
-$turmaBolo->colocarNoForno($tempoDeForno);
+
+$outroBolo = new FormaBolo();
+$outroBolo->transferirIngredientes("Chocolate", $turmaBolo);
+
+$turmaBolo->fazerBolo();
--- murilo_veetorazo/poo/src/FormaBolo.php
+++ murilo_veetorazo/poo/src/FormaBolo.php
@@ -19,12 +19,18 @@ class FormaBolo
             return;
         }
 
-        echo "Misturando os ingredientes ". json_encode($this->ingredientes);
+        echo "Misturando os ingredientes <br>". json_encode($this->ingredientes);
     }
 
     public function colocarNoForno(int $tempoForno = 1): void 
     {
         $this->tempoForno = $tempoForno;
         echo "Colocando no forno o bolo, tempo para ficar pronto é de $this->tempoForno horas <br>";
+    }
+    public function transferirIngredientes(string $ingrediente, FormaBolo $boloAReceberIngrediente): void 
+    {
+         array_push($boloAReceberIngrediente->ingredientes, $ingrediente);
+
+         echo "Ingredientes enviados para o outro bolo <br>";
     }
 }
